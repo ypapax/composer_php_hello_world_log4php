b39dc3e9de84fb4cc439b3fb307c16293619861b
Maxim Yefremov <maxYefr@gmail.com>
2019-09-27 12:57:44 +0300

    added configurator

--- src/HelloWorld/greetings.php
+++ src/HelloWorld/greetings.php
@@ -4,15 +4,42 @@ namespace HelloWorld;
 
 use SebastianBergmann\Timer\Timer;
 use Logger;
+use LoggerConfigurator;
+use LoggerHierarchy;
+use LoggerAppenderConsole;
 
 class Greetings
 {
 	public static function sayHelloWorld()
 	{
-		$log = Logger::getLogger("foo");
+		Logger::configure([], new AppLogConfigurator); // use the specified configuration
+		$log = Logger::getLogger("general");
 		$log->info("Yay! Works!");
 		$timer = new Timer();
 		$timer::start();
 		return 'Hello World\n' . $timer->resourceUsage() . "\n";
 	}
+}
+
+class AppLogConfigurator implements LoggerConfigurator
+
+{
+
+	public function configure(LoggerHierarchy $hierarchy, $input = null)
+
+	{
+
+		$consoleAppender = new LoggerAppenderConsole(); // create a new console appender
+
+
+		$consoleAppender->activateOptions(); // activate the appender
+
+
+		$rootLogger = $hierarchy->getRootLogger(); // holds multiple loggers if needed
+
+		$rootLogger->addAppender($consoleAppender); // add our appenders
+
+
+	}
+
 }
